99cb2efa9bbd0277d01f541d17cbace58fae6e65
--Explosion-- <explosion.scratch@gmail.com>
2022-12-29 18:59:36 -0600

    Initial Commit

---
+++ Authenticator/AuthenticatorApp.swift
@@ -0,0 +1,15 @@
+//Created by Explosion-Scratch
+
+import SwiftUI
+
+@main
+struct AuthenticatorApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
